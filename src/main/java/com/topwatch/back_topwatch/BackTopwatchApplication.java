08d79a1aafe0ac4cdcb2645a2dcc7847b96260ac
JuanSebastianB <j.becerrab@uniandes.edu.co>
2025-12-08 09:36:09 -0500

    feat: init project

---
+++ src/main/java/com/topwatch/back_topwatch/BackTopwatchApplication.java
@@ -0,0 +1,13 @@
+package com.topwatch.back_topwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackTopwatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackTopwatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/topwatch/back_topwatch/BackTopwatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topwatch.back_topwatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackTopwatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
